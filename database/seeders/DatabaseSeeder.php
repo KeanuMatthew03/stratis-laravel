be4e9bae87d88cd6c5dd589c2b201c1ca18776ff
KeanuMatthew03 <162323901+KeanuMatthew03@users.noreply.github.com>
2026-07-22 13:13:40 +0700

    feat: populate ERD tables from dataset.json in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,43 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             VisitorStatisticSeeder::class,
-            SumberSeeder::class,
-            NegaraSeeder::class,
-            KunjunganSeeder::class,
         ]);
+
+        $json = file_get_contents(storage_path('app/dataset.json'));
+        $data = json_decode($json, true);
+        
+        $sumberMap = [];
+
+        foreach ($data as $item) {
+            // Assume sumber is generic for now if not provided, or create one dummy sumber
+            if (empty($sumberMap['BPS'])) {
+                $sumber = \App\Models\Sumber::create(['nama_sumber' => 'BPS']);
+                $sumberMap['BPS'] = $sumber->id_sumber;
+            }
+
+            $negara = \App\Models\Negara::create([
+                'nama_negara' => $item['country_name'],
+                'id_sumber' => $sumberMap['BPS']
+            ]);
+
+            $months = [
+                'Januari' => 'jan',
+                'Februari' => 'feb',
+                'Maret' => 'mar',
+                'April' => 'apr',
+                'Mei' => 'may'
+            ];
+
+            foreach ($months as $bulanIndo => $bulanEng) {
+                if (isset($item[$bulanEng])) {
+                    \App\Models\Kunjungan::create([
+                        'jumlah' => $item[$bulanEng],
+                        'bulan' => $bulanIndo,
+                        'id_negara_asal' => $negara->id_negara,
+                        'id_negara_tujuan' => null // Nullable for generic
+                    ]);
+                }
+            }
+        }
     }
 }
